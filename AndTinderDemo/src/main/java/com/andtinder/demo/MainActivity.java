4d722ca9cbf0557edd9513d9b444622a4c78090d
hongwang.song <hongwang.song@dian.fm>
2015-12-23 16:40:23 +0800

    dev开发

--- AndTinderDemo/src/main/java/com/andtinder/demo/MainActivity.java
+++ AndTinderDemo/src/main/java/com/andtinder/demo/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends Activity {
 
     /**
      * This variable is the container that will host our cards
-     * 开启了dev分支git
+     * dev
      */
 	private CardContainer mCardContainer;
 	
